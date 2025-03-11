464f77ee0d9fa58edaf5b7c5d77ec23b70f4d605
efhan-the-unorthodox <53419134+efhan-the-unorthodox@users.noreply.github.com>
2025-03-11 16:16:48 +0800

    Leo's Updates

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, We are Grp 5 Guys this is our OODP project!");
         System.out.println("This is Dehan");
+        System.out.println("This is Ethan");
     }
 }
