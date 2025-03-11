b2f710b0b1397a151c780519f0b84942760b9d68
Esther <estherleeyishan@gmail.com>
2025-03-11 15:50:41 +0800

    Init 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Guys this is our OODP project!");
+        System.out.println("Hello, We are Grp 5 Guys this is our OODP project!");
     }
 }
